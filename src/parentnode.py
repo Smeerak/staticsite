c00c9e621e834e03531276ea9fb316ba87313f17
smeerak <kareem.elhawary@gmail.com>
2025-05-24 00:20:20 -0300

    L5 - initial check in

---
+++ src/parentnode.py
@@ -0,0 +1,16 @@
+from HTMLNode import HTMLNode
+
+class ParentNode(HTMLNode):
+
+    def __init__(self, tag: str = None, children: list = None, props: dict = None) -> None:
+        super().__init__(tag, children, props)
+
+    def to_html(self):
+        if(self.tag):
+            #recurse
+            pass
+        else:
+            raise ValueError("No tag found")
+        
+        if(self.children == None):
+            raise ValueError("No children found")
